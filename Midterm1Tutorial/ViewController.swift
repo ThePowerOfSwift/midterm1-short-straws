f42f5b616f49ab95ce33f317cb2700c7326603fa
Greg Schloemer <gregs2011@gmail.com>
2019-09-19 13:19:28 -0400

    Re-structured files

---
+++ Midterm1Tutorial/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Midterm1Tutorial
+//
+//  Created by  on 9/19/19.
+//  Copyright © 2019 CS485G Team 1. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
