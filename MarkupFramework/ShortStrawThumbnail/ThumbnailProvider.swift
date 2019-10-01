66ac2bcda5d96037e1c43a0c28234728e5c26002
Luke Ritchie <Ritchieluk@gmail.com>
2019-09-30 21:24:03 -0400

    Fixed Thumbnails

--- MarkupFramework/ShortStrawThumbnail/ThumbnailProvider.swift
+++ MarkupFramework/ShortStrawThumbnail/ThumbnailProvider.swift
@@ -12,38 +12,41 @@ import MarkupFramework
 
 class ThumbnailProvider: QLThumbnailProvider {
     
-    override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
-        handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
+    override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void)
+    {
+        handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing:
+            { () -> Bool in
           
-          var result = true
-          do {
-            // 1
+            var result = true
+            do {
+            // the file URL is inside the request as a property, we can get it's description using an unarchiver
             let data = try Data(contentsOf: request.fileURL)
             let unarchiver = try NSKeyedUnarchiver(forReadingFrom: data)
             unarchiver.requiresSecureCoding = false
             if let content = unarchiver.decodeObject(of: ContentDescription.self,
-                                                     forKey: NSKeyedArchiveRootObjectKey) {
+                                                     forKey: NSKeyedArchiveRootObjectKey)
+            {
+                // we create a plugin view, this was part of the starter project
+                let template = PluginViewFactory.plugin(named: content.template)
               
-              // 2
-              let template = PluginViewFactory.plugin(named: content.template)
+                // we can configure the UIView object with size information
+                template.view.frame = CGRect(origin: .zero, size: request.maximumSize)
+                template.update(content)
+                  
+                // We then call draw to create it right on the current context
+                template.view.draw(template.view.bounds)
               
-              // 3
-              template.view.frame = CGRect(origin: .zero, size: request.maximumSize)
-              template.update(content)
-              
-              // 4
-              template.view.draw(template.view.bounds)
-              
-            } else {
-              result = false
             }
-          }
-          catch {
-            result = false
-          }
+            else {
+                result = false
+            }
+            }
+            catch {
+                result = false
+            }
           
-          return result
+        return result
         }), nil)
         
-            }
+    }
 }
